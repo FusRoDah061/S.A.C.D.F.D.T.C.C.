1b76adf47e339ed734195bcd054ef9932eb2b5c2
Allex Rodrigues <amaral.allex@gmail.com>
2017-05-12 22:10:17 -0300

    Adicionado header de conversões de tipos

---
+++ parse.h
@@ -0,0 +1,36 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+/*Converte um real para pointeiro char
+ *
+ * Parâmetros:
+ * (float) n: número a ser convertido
+ *
+ * Retorna:
+ * (char*) string de n
+ */
+char *float_to_char(float n)
+{
+	 char *s = malloc(sizeof(char));
+
+	 sprintf(s, "%f", n);
+
+     return s;
+}
+
+/*Converte um inteiro para pointeiro char
+ *
+ * Parâmetros:
+ * (int) n: número a ser convertido
+ *
+ * Retorna:
+ * (char*) string de n
+ */
+ char *int_to_char(int n)
+ {
+	 char *s = malloc(sizeof(char) * sizeof(int));
+
+     sprintf(s, "%d", n);
+
+     return s;
+ }
