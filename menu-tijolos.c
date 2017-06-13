6209506f04a2ec7a1cc8736d9e7e299f6f627aa4
leticiasayuri <leticiasyrk@gmail.com>
2017-06-13 17:24:26 -0300

    Menu simples com as opções de tijolos.

---
+++ menu-tijolos.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include <locale.h>
+
+main(){
+    //setlocale(LC_ALL, "");
+
+    printf("\n--------------------\n");
+    printf("Tipos de Tijolo\n");
+    printf("--------------------\n");
+
+    printf("\nSelecione o tipo do tijolo: \n\n");
+
+    printf(" 1 - Blocos\n");
+    printf(" 2 - Tijolos Cerâmicos\n");
+    printf(" 3 - Tijolos Maciços\n");
+    printf(" 4 - Outra opção\n\n");
+
+    system("pause");
+
+}
